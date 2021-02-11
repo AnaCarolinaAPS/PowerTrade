f47f07620e8a07492bf74ce96327a29c57f8e5d6
Ana Carolina <anacarolinaaps@gmail.com>
2021-02-11 10:07:25 -0300

    Update of meta tags

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="image_src" href="http://www.powertrade.com.py/img/PowerTradeLogo.png">
     <!-- MS Tile - for Microsoft apps-->
     <meta name="msapplication-TileImage" content="http://www.powertrade.com.py/img/PowerTradeLogo.png">
     {{-- for google --}}
